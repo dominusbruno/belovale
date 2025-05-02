f331d8ae4e637070260e60e00e15c54edd9667b2
Bruno Rodrigues <dominusbruno@gmail.com>
2025-05-02 00:28:59 -0300

    Páginas reorganizadas

---
+++ js/alerta.js
@@ -0,0 +1,30 @@
+export function mostrarAlerta(mensagem, tipo = 'success') {
+  const alerta = document.getElementById('alertaCustom');
+  const texto = document.getElementById('textoAlerta');
+
+  texto.textContent = mensagem;
+
+  // Reseta classes visuais
+  alerta.className = `fixed bottom-4 left-1/2 -translate-x-1/2 px-6 py-3 rounded text-white text-sm shadow-lg z-50 ${
+    tipo === 'error' ? 'error' : 'success'
+  } hidden`;
+
+  // Remove hidden com delay de renderização
+  requestAnimationFrame(() => {
+    alerta.classList.remove('hidden'); // torna visível
+    requestAnimationFrame(() => {
+      alerta.classList.add('mostrar'); // aplica a animação de subida
+
+      // Inicia processo de ocultar
+      setTimeout(() => {
+        alerta.classList.remove('mostrar');
+        alerta.classList.add('ocultar');
+
+        setTimeout(() => {
+          alerta.classList.remove('ocultar');
+          alerta.classList.add('hidden');
+        }, 400); // tempo da animação de saída
+      }, 2000); // tempo que fica visível
+    });
+  });
+}
